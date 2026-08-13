ac028e714b0824d8068474d6755c82cff7b201e0
Enise Z. <65121927+drkbluescience@users.noreply.github.com>
2026-08-13 16:18:20 +0300

    refactor menu and job operations into functions

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def main():
+applications = []
 
+def add_job(company, position, status):
+    job = {
+    "company": company,
+    "position": position,
+    "status": status
+}
+
+    applications.append(job)
+
+
+def list_jobs():
+    if applications:
+        for i, v in enumerate(applications, start=1):
+            print(f"{i}. {v['company']} - {v['position']} - {v['status']}")
+
+    else:
+        print("\nThere is no application!\n")   
+    
+
+def show_menu():
     txt = """
 === Job Application Tracker ===
 1. Add Job"
@@ -8,21 +28,50 @@ def main():
 
 Select an option:
 """
+    slt = input(f"{txt}")
 
-    slt = int(input(f"{txt}"))
+    if slt.isdigit():
+        slt = int(slt)
+        if if 0 < slt < 4:
+            return slt
+        else:
+            return None
     
-    if slt == 1:
-        print("Add Job selected")
-    elif slt == 2:
-        print("List Jobs selected")
-    elif slt == 3:
-        print("Goodbye!")
     else:
         print("Invalid option")
+        return None
 
-    print(slt)
+
+def main():
+    slt = show_menu()
+
+    if slt is not None:
+        if slt == 1:
+            print("Add Job selected")
+            
+            company = input("Company: ")
+            position = input("Position: ")
+            status = input("Status: ")
+
+            add_job(company, position, status)
+            print("\nJob added successfully.")
+
+        elif slt == 2:
+            print("List Jobs selected")
+            list_jobs()
+
+        elif slt == 3:
+            print("Goodbye!")
+
+    return slt
 
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    res = None
+
+    while True:
+        res = main()
+        if res == 3:
+            break
 
